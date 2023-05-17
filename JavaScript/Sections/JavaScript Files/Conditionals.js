675339251e539f1dd713d252952af7e01f18b81b
OllyBjorck <bjorckoliver@gmail.com>
2023-05-17 19:24:51 +0100

    covered if statements

---
+++ JavaScript/Sections/JavaScript Files/Conditionals.js
@@ -0,0 +1,7 @@
+console.log("before conditional")
+let random = Math.random()
+if(random < 0.5) { //if statements allow you to create conditional code that runs based on the conditions set within the statement
+    console.log("your number is less than 0.5") //if the if statement is found to be correct, the code inside will run
+    console.log(random); //if it isn't correct, the code will not run
+}
+console.log("after conditional")
