5351f1ee51d012ec7cdfa2c4adc137a9df726000
OllyBjorck <bjorckoliver@gmail.com>
2023-05-17 19:31:38 +0100

    completed Our First Conditional Exercise

---
+++ JavaScript/Exercises/7.Our-First-Conditional-Exercise.js
@@ -0,0 +1,9 @@
+//Exercise completed in Udemy IDE and copied here
+
+function isEven(num){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+   if(num%2 === 0) {
+       console.log("even");
+   }
+    //AND THIS LINE ↑↑↑↑↑
+}
