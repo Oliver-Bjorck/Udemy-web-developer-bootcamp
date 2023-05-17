98feafcaef4ff098abeefaae50132f465439fbb3
OllyBjorck <bjorckoliver@gmail.com>
2023-05-17 18:20:00 +0100

    completed More String Methods Practice

---
+++ JavaScript/Exercises/5.More-String-Methods-Practice.js
@@ -0,0 +1,7 @@
+//Exercise completed in Udemy IDE and copied here
+
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let facialHair = word.slice(5).replace('o', 'e');
