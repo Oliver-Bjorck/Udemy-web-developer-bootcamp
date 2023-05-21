4829c96eea7571f9cbfc4db56225bdd99049e235
OllyBjorck <bjorckoliver@gmail.com>
2023-05-21 18:59:10 +0100

    completed Array Access Exercise

---
+++ JavaScript/Exercises/12.Array-Access-Exercise.js
@@ -0,0 +1,7 @@
+//Exercise completed in Udemy IDE and copied here
+
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+
+// YOUR CODE BELOW HERE:
+leaderboard[1] = 'Luna';
+leaderboard[3] = 'Draco';
