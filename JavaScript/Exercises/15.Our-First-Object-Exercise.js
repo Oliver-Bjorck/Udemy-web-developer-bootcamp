7d3662587a9d2ba1034ec76a3e87c837d8ea476a
OllyBjorck <bjorckoliver@gmail.com>
2023-05-31 18:29:56 +0100

    completed Our First Object Exercise

---
+++ JavaScript/Exercises/15.Our-First-Object-Exercise.js
@@ -0,0 +1,9 @@
+//Exercise completed in Udemy IDE and copied here
+
+// Your code goes in here:
+const product = {
+    name:"Gummy Bears",
+    inStock: true,
+    price: 1.99,
+    flavors: ["grape", "apple", "cherry"]
+}
