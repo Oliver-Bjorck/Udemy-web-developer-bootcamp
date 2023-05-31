58d3f7afb078aca2c97d666bf5e1f58d794fe577
OllyBjorck <bjorckoliver@gmail.com>
2023-05-31 17:57:58 +0100

    updated Nested Arrays Exercise

---
+++ JavaScript/Exercises/14.Nested-Arrays-Exercise.js
@@ -0,0 +1,13 @@
+//Exercise completed in Udemy IDE and copied here
+
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+airplaneSeats[3][1] = "Hugo";
