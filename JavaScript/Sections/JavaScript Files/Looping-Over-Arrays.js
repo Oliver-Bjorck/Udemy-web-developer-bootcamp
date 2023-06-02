b545af957dcfeaa05ed4a97270753491f9d5dbe3
OllyBjorck <bjorckoliver@gmail.com>
2023-06-02 18:15:25 +0100

    covered looping over arrays

---
+++ JavaScript/Sections/JavaScript Files/Looping-Over-Arrays.js
@@ -0,0 +1,19 @@
+const animals = [
+    "Aardvark",
+    "Aardwolf",
+    "African Elephant",
+    "African Tree Pangolin",
+    "Alligator",
+    "Alpaca",
+    "Anteater",
+    "Antelope",
+    "Ape(s)",
+    "Arabian Horse",
+    "Armadillo",
+    "Arthropod(s)",
+    "Asian Elephant"
+]
+
+for (let i = 0; i < animals.length; i++) { //for loops are quite commonly used to cycle through objects in an array
+    console.log(i, animals[i]);
+}
