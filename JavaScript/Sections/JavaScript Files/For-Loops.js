223ae80725695d2eec32f6d6bbd92d743cd60582
OllyBjorck <bjorckoliver@gmail.com>
2023-06-02 17:38:34 +0100

    covered intro to for loops

---
+++ JavaScript/Sections/JavaScript Files/For-Loops.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <=10; i++) { //a for loop can be used to repeat code based on a set of rules
+    console.log(i); //this code prints the numbers 1 through 10
+}
+
+//the syntax for a for loop is:
+//for ([initial expression]; [condition]; [increment expression])
