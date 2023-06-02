3ee680993ae625ce32ee38a23572aed6d521ee1f
OllyBjorck <bjorckoliver@gmail.com>
2023-06-02 17:41:23 +0100

    completed Our First For Loop Practice

---
+++ JavaScript/Exercises/17.Our-First-For-Loop-Practice.js
@@ -0,0 +1,6 @@
+//Exercise completed in Udemy IDE and copied here
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+for (let i = 1; i <=6; i++) {
+    console.log("Da ba dee da ba daa");
+}
