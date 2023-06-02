f9645b2b3b526bbf10506013aea16185eab7b0f0
OllyBjorck <bjorckoliver@gmail.com>
2023-06-02 17:51:05 +0100

    completed more for loop examples

--- JavaScript/Sections/JavaScript Files/For-Loops.js
+++ JavaScript/Sections/JavaScript Files/For-Loops.js
@@ -4,3 +4,12 @@ for (let i = 1; i <=10; i++) { //a for loop can be used to repeat code based on 
 
 //the syntax for a for loop is:
 //for ([initial expression]; [condition]; [increment expression])
+
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i);
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
