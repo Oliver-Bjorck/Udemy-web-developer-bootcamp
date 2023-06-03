114a182e339ce76901c55615cc558b5aa388c4e2
OllyBjorck <bjorckoliver@gmail.com>
2023-06-04 00:09:03 +0100

    covered intro to functions

---
+++ JavaScript/Sections/JavaScript Files/Intro-To-Functions.js
@@ -0,0 +1,7 @@
+function singSong() { //functions are used to make code that can be called upon repeatedly
+    console.log("Do")
+    console.log("Re")
+    console.log("Mi")
+}
+
+singSong(); //the code within a function won't be run until the function is called
