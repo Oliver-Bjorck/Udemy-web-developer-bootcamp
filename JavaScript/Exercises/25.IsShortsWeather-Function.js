f0432817d92bc22803d9a352d1dc6b80ea5168b9
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 17:47:02 +0100

    completed isShortsWeather function exercise

---
+++ JavaScript/Exercises/25.IsShortsWeather-Function.js
@@ -0,0 +1,10 @@
+//Exercise completed in Udemy IDE and copied here
+
+// DEFINE YOUR FUNCTION BELOW:
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    } else {
+        return false;
+    }
+}
