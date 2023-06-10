19df4b38a96df4aaaaee9fb53f61ae7d086f7c87
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 22:33:24 +0100

    covered block scoping

---
+++ JavaScript/Sections/JavaScript Files/Block-Scope.js
@@ -0,0 +1,8 @@
+let radius = 8;
+if (radius > 0) {  
+    const PI = 3.14159; //variables can also be scoped to a block (i.e. conditionals, loops, anything contained within curly braces)
+    var msg = 'HIII!'; //the var keyword can override this, however this is an outdated keyword
+}
+
+console.log(radius);
+console.log(PI);
