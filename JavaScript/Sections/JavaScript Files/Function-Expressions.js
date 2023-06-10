3e00e2e992c12e588751a4e44a5e0d0889273b2c
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 23:33:12 +0100

    covered function expressions

---
+++ JavaScript/Sections/JavaScript Files/Function-Expressions.js
@@ -0,0 +1,3 @@
+const add = function (x, y) { //functions can also be stored within variables
+    return x + y; //it's mostly just another syntax and doesn't differ much from ordinary function syntax
+}
