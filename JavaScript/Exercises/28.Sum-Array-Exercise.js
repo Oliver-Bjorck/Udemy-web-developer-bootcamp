b1e68b79bf673b192550173edf5a4ab98ef0e114
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 18:02:57 +0100

    completed sum array exercise

---
+++ JavaScript/Exercises/28.Sum-Array-Exercise.js
@@ -0,0 +1,10 @@
+//Exercise completed in Udemy IDE and copied here
+
+// DEFINE YOUR FUNCTION BELOW:
+function sumArray(array) {
+    let total = 0;
+    for(let i = 0; i <array.length; i++) {
+        total += array[i];
+    }
+    return total;
+}
