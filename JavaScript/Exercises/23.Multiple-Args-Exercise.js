195cf136d5b49254e5a8853a89e53dcdad20b005
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 17:32:16 +0100

    completed multiple args exercise

---
+++ JavaScript/Exercises/23.Multiple-Args-Exercise.js
@@ -0,0 +1,10 @@
+//Exercise completed in Udemy IDE and copied here
+
+// define isSnakeEyes below:
+function isSnakeEyes(dieRoll1, dieRoll2) {
+    if (dieRoll1 === 1 && dieRoll2 === 1) {
+        console.log('Snake Eyes!');
+    } else {
+        console.log('Not Snake Eyes!');
+    }
+}
