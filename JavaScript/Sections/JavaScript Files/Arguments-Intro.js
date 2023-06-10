314ccf9f0ad26e278616861cbf50c83521cec282
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 17:14:04 +0100

    covered intro to arguments

---
+++ JavaScript/Sections/JavaScript Files/Arguments-Intro.js
@@ -0,0 +1,3 @@
+function greet(firstName) { //arguments allow us to pass through variables into our functions
+    console.log(`Hey there, ${firstName}!`); //thus creating further reusability of our functions
+}
