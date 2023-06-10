aa496e201f321bb8594c54151b13f5db14d918cb
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 17:16:28 +0100

    completed rant exercise

---
+++ JavaScript/Exercises/22.Rant-Exercise.js
@@ -0,0 +1,9 @@
+//Exercise completed in Udemy IDE and copied here
+
+// DEFINE YOUR FUNCTION:
+
+function rant(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
