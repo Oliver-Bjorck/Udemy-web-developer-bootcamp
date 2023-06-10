a4c9d5a08ec2dd73f997e53fa5c888faf8977f41
OllyBjorck <bjorckoliver@gmail.com>
2023-06-10 23:35:15 +0100

    completed function expression exercise

---
+++ JavaScript/Exercises/30.Function-Expression-Exercise.js
@@ -0,0 +1,7 @@
+//Exercise completed in Udemy IDE and copied here
+
+//NOTE: Udemy's coding exercise platform does NOT support the ** operator.  You'll need to multiply a number by itself or use the Math.pow() method.
+
+const square = function (num) {
+    return num * num;
+}
