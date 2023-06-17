1a12fff19d0021c5fcf333735e37d5e48a45b0a8
OllyBjorck <bjorckoliver@gmail.com>
2023-06-17 19:57:22 +0100

    covered defining methods

---
+++ JavaScript/Sections/JavaScript Files/Defining-Methods.js
@@ -0,0 +1,9 @@
+const myMath = {
+    PI: 3.14159,
+    square: function (num) { //methods are simply a function that's been added as a property on an onject
+        return num * num;
+    },
+    cube(num) { //there is a shorthand way of implementing methods as well
+        return num ** 3;
+    }
+}
