00f46c87f81c360b20d85ffdc94f02cda1585ef5
OllyBjorck <bjorckoliver@gmail.com>
2023-06-17 20:17:40 +0100

    completed Egg Laying Exercise

---
+++ JavaScript/Exercises/32.Egg-Laying-Exercise.js
@@ -0,0 +1,10 @@
+//Exercise completed in Udemy IDE and copied here
+
+const hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount += 1;
+        return "EGG";
+    }
+}
