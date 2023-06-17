cf0a446ef0c12d42c3d33954b69704b4b7f6d781
OllyBjorck <bjorckoliver@gmail.com>
2023-06-17 19:59:50 +0100

    completed Methods exercise

---
+++ JavaScript/Exercises/31.Methods-Exercise.js
@@ -0,0 +1,10 @@
+//Exercise completed in Udemy IDE and copied here
+
+const square = {
+    area(side) {
+        return side * side;
+    },
+    perimeter(side) {
+        return side * 4;
+    }
+}
