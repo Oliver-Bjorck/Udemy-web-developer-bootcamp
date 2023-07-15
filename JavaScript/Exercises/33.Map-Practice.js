b1ce0954059484b708afaf8df9b11ad7e8a2a2fd
OllyBjorck <bjorckoliver@gmail.com>
2023-07-15 18:40:21 +0100

    completed Map Practice exercise

---
+++ JavaScript/Exercises/33.Map-Practice.js
@@ -0,0 +1,9 @@
+//Exercise completed in Udemy IDE and copied here
+
+// DO NOT ALTER THE FOLLOWING CODE:
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+// Write your code here
+const firstNames = fullNames.map(function(name) {
+    return name.first;
+})
