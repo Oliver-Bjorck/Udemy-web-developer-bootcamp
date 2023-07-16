ae85dd8ee7c442d0834b3f6c6f8a9a801a5e32d8
OllyBjorck <bjorckoliver@gmail.com>
2023-07-16 15:23:12 +0100

    covered spread in function calls

---
+++ JavaScript/Sections/JavaScript Files/Spread-in-Function-Calls.js
@@ -0,0 +1,4 @@
+const nums = [13, 4, 5, 21, 3, 3, 1, 2, 7, 6, 4, 2, 53456]
+
+Math.max(...nums) //the spread syntax allows for elements within an array to spread across a function call
+//If we were to simply input the array, it would show NaN as it takes the array as a whole, rather than the elements within
