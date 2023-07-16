964c2358818a418a1f6bf0be88d948992da3a1a2
OllyBjorck <bjorckoliver@gmail.com>
2023-07-16 16:37:19 +0100

    covered destructuring arrays

---
+++ JavaScript/Sections/JavaScript Files/Destructuring-Arrays.js
@@ -0,0 +1,3 @@
+const scores = [929321, 899341, 888336, 772739, 543671, 243567, 111934];
+
+const [gold, silver, bronze, ...everyoneElse] = scores; //destructuring an array simply assigns an array element to a new variable based on its order
