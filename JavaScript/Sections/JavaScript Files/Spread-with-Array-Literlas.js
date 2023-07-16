24c552f66d4dbb1d1902e1c7aa0e24bf908d3df0
OllyBjorck <bjorckoliver@gmail.com>
2023-07-16 15:29:51 +0100

    covered spread with array literals

---
+++ JavaScript/Sections/JavaScript Files/Spread-with-Array-Literlas.js
@@ -0,0 +1,4 @@
+const cats = ['Blue', 'Scout', 'Rocket'];
+const dogs = ['Rusty', 'Wyatt'];
+
+const allPets = [...cats, ...dogs]; //Spread can also be used to take elements from other arrays and copy them into a new array
