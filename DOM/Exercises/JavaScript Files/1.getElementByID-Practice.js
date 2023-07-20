d97509fe98ef434e2ccfba60ac238cd620274cfb
OllyBjorck <bjorckoliver@gmail.com>
2023-07-20 20:01:02 +0100

    completed getElementById practice

---
+++ DOM/Exercises/JavaScript Files/1.getElementByID-Practice.js
@@ -0,0 +1,5 @@
+//Exercise completed in Udemy IDE and copied here
+
+// Write your code in here:
+const image = document.getElementById('unicorn')
+const heading = document.getElementById('mainheading')
