62666c3442335df656ef8ba165cd9ed328d788bc
OllyBjorck <bjorckoliver@gmail.com>
2023-07-22 22:38:17 +0100

    completed querySelector Practice

---
+++ DOM/Exercises/JavaScript Files/2.querySelector-Practice.js
@@ -0,0 +1,6 @@
+//Exercise completed in Udemy IDE and copied here
+
+// Your code goes in here!
+const doneTodos = document.querySelectorAll('.done');
+
+const checkbox = document.querySelector('input[type="checkbox"]')
