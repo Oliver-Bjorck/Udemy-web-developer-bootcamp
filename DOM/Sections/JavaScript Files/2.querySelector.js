782a47631be090e7e2a4f69d983dabf1ef68b274
OllyBjorck <bjorckoliver@gmail.com>
2023-07-22 22:28:09 +0100

    covered querySelector and querySelectorAll

---
+++ DOM/Sections/JavaScript Files/2.querySelector.js
@@ -0,0 +1,4 @@
+const javaAnchor = document.querySelector('a[title="Java"]') //querySelector is a faster way to select an element based on their id, class, tag, whatever really
+//querySelector will only select the first element that matches the parameters
+
+const allParagraphs = document.querySelectorAll('p') //querySelectorAll will create an HTMLCollection with all elements matching the parameters
