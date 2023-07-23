3d1b46062b48b9f0918bca8e5f8326b1a3a7e500
OllyBjorck <bjorckoliver@gmail.com>
2023-07-23 18:09:28 +0100

    completed magical forest circle exercise

---
+++ DOM/Exercises/JavaScript Files/5.Magical-Forest-Circle-Exercise.js
@@ -0,0 +1,10 @@
+//Exercise completed in Udemy IDE and copied here
+
+const container = document.querySelector('#container');
+
+container.style.textAlign = 'center';
+
+const treeImage = document.querySelector('img');
+
+treeImage.style.width = '150px';
+treeImage.style.borderRadius = '50%';
