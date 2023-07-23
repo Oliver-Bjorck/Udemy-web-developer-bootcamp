e0c516c5292a27398fe31cf56dbda0b2e4705fec
OllyBjorck <bjorckoliver@gmail.com>
2023-07-23 17:39:35 +0100

    completed manipulating attributes pratice

---
+++ DOM/Exercises/JavaScript Files/4.Manipulating-Attributes-Practice.js
@@ -0,0 +1,7 @@
+//Exercise completed in Udemy IDE and copied here
+
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+const eggImage = document.querySelector('img');
+eggImage.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+eggImage.setAttribute('alt', 'chicken');
