1b2b09298aee93980b6dc78ea77df52dc13987bc
OllyBjorck <bjorckoliver@gmail.com>
2023-08-02 15:11:04 +0100

    practised with form events and preventDefault

--- DOM/Sections/JavaScript Files/15.Form-Events-&-PreventDefault.js
+++ DOM/Sections/JavaScript Files/15.Form-Events-&-PreventDefault.js
@@ -1,5 +1,13 @@
 const form = document.querySelector('#shelterForm');
-form.addEventListener('submit', function(e) {
-    console.log('Submitted!');
+const input = document.querySelector('#catName');
+const list = document.querySelector('#cats')
+
+form.addEventListener('submit', function(e) { //Event Listeners can be especially important in forms
     e.preventDefault(); //preventDefault stops an event from triggering its default action, in this case changing to a new page
+    console.log(input.value);
+    const catName = input.value; //they can be used to read an input and insert them where they're needed
+    const newLi = document.createElement('Li'); //such as a list
+    newLi.innerText = catName;
+    list.append(newLi);
+    input.value = '';
 })
