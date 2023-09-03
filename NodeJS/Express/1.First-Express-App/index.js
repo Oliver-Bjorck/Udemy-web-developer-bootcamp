040a8828a7b722489a6fba6a9d85e7a05b58f4af
OllyBjorck <bjorckoliver@gmail.com>
2023-09-03 19:53:55 +0100

    covered the request and response objects

--- NodeJS/Express/1.First-Express-App/index.js
+++ NodeJS/Express/1.First-Express-App/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(() => { //app.use will run the code below anytime we get a request from the server
+app.use((req, res) => { //app.use will run the code below anytime we get a request from the server
+    //express creates the req and res JS objects by parsing the request and response information from the server
     console.log("We got a new request")
+    res.send('<h1>This is my webpage</h1>') //res.send sends the HTTP response
 })
 
 app.listen(3000, () => { //app.listen will run the code below when the server on the designated port has started
