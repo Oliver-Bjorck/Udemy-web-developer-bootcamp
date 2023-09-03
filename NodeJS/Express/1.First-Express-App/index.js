5e09102a0a33cad8e83c3e3a4b7711e4b0946505
OllyBjorck <bjorckoliver@gmail.com>
2023-09-04 00:51:42 +0100

    covered path parameters

--- NodeJS/Express/1.First-Express-App/index.js
+++ NodeJS/Express/1.First-Express-App/index.js
@@ -6,8 +6,19 @@ const app = express();
 //     console.log("We got a new request")
 //     res.send('<h1>This is my webpage</h1>') //res.send sends the HTTP response
 // })
+
 app.get('/', (req, res) => { //app.get routes HTTP GET requests to the specified path
     res.send('This is the home page!')
+})
+
+app.get('/r/:subreddit', (req, res) => { //this syntax can be used to create a path pattern that te server can recognise
+    const {subreddit} = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => { //the colon indicates a variable rather than hard coded text
+    const {subreddit, postId} = req.params;
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`)
 })
 
 app.post('/cats', (req, res) => { //app.post routes HTTP POST requests to the specified path
