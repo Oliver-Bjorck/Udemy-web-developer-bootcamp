8848760141e9311b04f06c904cbc0815f42118f0
OllyBjorck <bjorckoliver@gmail.com>
2023-09-06 18:04:10 +0100

    created a subreddit template

--- NodeJS/Express/2.Templating/index.js
+++ NodeJS/Express/2.Templating/index.js
@@ -5,10 +5,14 @@ const path = require("path");
 app.set("view engine", "ejs"); //this will set the view engine to ejs, meaning we don't need to require it like other npm packages
 app.set("views", path.join(__dirname, "/views")); //this line of code allows us to access the home.ejs file from any directory
 
-app.get('/', 
-(req, res) => {
-    res.render("home.ejs") //this will render the ejs file as the html template
+app.get('/', (req, res) => {
+    res.render("home.ejs"); //this will render the ejs file as the html template
 }) //express searches automatically fora 'views' directory as default
+
+app.get("/r/:subreddit", (req, res) => {
+    const { subreddit } = req.params;
+    res.render("subreddit", {subreddit});
+})
 
 app.get("/random", (req, res) => {
     const randomNum = Math.floor(Math.random() * 10) + 1;
