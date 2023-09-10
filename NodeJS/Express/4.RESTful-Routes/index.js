a6ed419174cf03604f43110a8c5c8d2479205927
OllyBjorck <bjorckoliver@gmail.com>
2023-09-10 18:58:54 +0100

    created a comments index

--- NodeJS/Express/4.RESTful-Routes/index.js
+++ NodeJS/Express/4.RESTful-Routes/index.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true })); //this middleware parses the express body as urlencoded data
 app.use(express.json()); //whereas this middleware parses the express body as JSON
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const comments = [
+    {
+        username: "Todd",
+        comment: "lol that is so funny!"
+    },
+    {
+        username: "Skyler",
+        comment: "I like to go birdwatching with my dog"
+    },
+    {
+        username: "Sk8erBoi",
+        comment: "Plz delete your account, Todd"
+    },
+    {
+        username: "onlysayswoof",
+        comment: "woof woof woof"
+    }
+]
+
+app.get("/comments", (req, res) => {
+    res.render("comments/index", {comments});
+})
 
 app.get("/tacos", (req, res) => {
     res.send("Get /tacos response");
