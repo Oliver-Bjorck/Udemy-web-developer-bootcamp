d710bd79b0d4f814777773fb6e0dc7d82b7e7395
OllyBjorck <bjorckoliver@gmail.com>
2023-09-24 19:31:35 +0100

    covered mongoose virtuals

---
+++ MongoDB/Mongoose/person.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/shopApp')
+    .then(() => {
+        console.log("Connection Open!");
+    })
+    .catch(err => {
+        console.log("Oh No Error!");
+        console.log(err);
+    })
+
+const personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+});
+
+personSchema.virtual("fullName").get(function() { //virtuals are properties that are not stored on MongoDB
+    return `${this.first} ${this.last}`
+})
+
+const Person = mongoose.model("Person", personSchema);
