57bc3e44c4489e84633f0737fa09efee9df1deb4
OllyBjorck <bjorckoliver@gmail.com>
2023-09-24 14:39:28 +0100

    covered insertMany() in mongoose

--- MongoDB/Mongoose/index.js
+++ MongoDB/Mongoose/index.js
@@ -16,5 +16,17 @@ const movieSchema = new mongoose.Schema({ //defines the schema which maps to a M
 });
 
 const Movie = mongoose.model("Movie", movieSchema); //creates a model class
-const amadeus = new Movie({title: "Amadeus", year: 1986, score: 9.2, rating: "R"}); //creates a new object in the movie model
+//const amadeus = new Movie({title: "Amadeus", year: 1986, score: 9.2, rating: "R"}); //creates a new object in the movie model
 //this hasn't saved it to the database though, to do so we need to .load the file in the Node repl and execute amadeus.save()
+
+Movie.insertMany([ //insertMany() works similarly to insertMany in MongoDB
+    {title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
+    {title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+    {title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
+    {title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
+    {title: 'Moonrise kingdom', year: 2012, score: 7.3, rating: 'PG-13'},
+])
+.then(data => { //this function does return a promise and doesn't require use to use the .save() function
+    console.log("It Worked!");
+    console.log(data);
+})
