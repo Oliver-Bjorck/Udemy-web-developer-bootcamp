e8710486395a87227f1253a9082c8b9802e797a3
OllyBjorck <bjorckoliver@gmail.com>
2023-09-24 19:44:47 +0100

    covered mongoose middleware

--- MongoDB/Mongoose/person.js
+++ MongoDB/Mongoose/person.js
@@ -17,4 +17,14 @@ personSchema.virtual("fullName").get(function() { //virtuals are properties that
     return `${this.first} ${this.last}`
 })
 
+personSchema.pre("save", async function() { //pre allows you to run code before some other code code runs
+    this.first = "Yo";
+    this.last = "Mama";
+    console.log("About to Save!");
+})
+
+personSchema.post("save", async function() { //post allows you to run code after something has run
+    console.log("Just Saved!");
+})
+
 const Person = mongoose.model("Person", personSchema);
