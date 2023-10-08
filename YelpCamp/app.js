1320bd73c7b632360ea943e8a0ea059b593e0e74
OllyBjorck <bjorckoliver@gmail.com>
2023-10-08 16:43:18 +0100

    created a basic model for YelpCamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,12 +1,33 @@
+//requirements
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
+//connecting to mongodb
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+//upon mongodb connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const app = express();
+
+//setting view engine and folder
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+})
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({title: "My Backgarden", description: "Cheap camping"});
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
---
+++ YelpCamp/models/campground.js
@@ -0,0 +1,13 @@
+//require mongoose
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//campground schema
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+module.exports = mongoose.model("Campground", CampgroundSchema);
