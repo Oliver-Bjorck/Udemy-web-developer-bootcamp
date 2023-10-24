9a259da1cbea6c67c33ac90a3ed963e833c0d13d
OllyBjorck <bjorckoliver@gmail.com>
2023-10-24 17:15:52 +0100

    removed some comments

--- NodeJS/Express/5.Middleware/index.js
+++ NodeJS/Express/5.Middleware/index.js
@@ -5,14 +5,14 @@ const morgan = require("morgan"); //morgan is an example of middleware
 //middleware are functions that run during the request response process
 
 app.use(morgan("tiny")); //morgan logs the status code and response time for each request
-// app.use((req, res, next) => { //it is important when using middleware to use next
-//     console.log("This is my first middleware!")
-//     return next(); //next will allow the server to send a response after using the middleware
-// })
-// app.use((req, res, next) => {
-//     console.log("This is my second middleware!")
-//     return next(); //next invokes the next matching middleware or route handler
-// })
+app.use((req, res, next) => { //it is important when using middleware to use next
+    console.log("This is my first middleware!")
+    return next(); //next will allow the server to send a response after using the middleware
+})
+app.use((req, res, next) => {
+    console.log("This is my second middleware!")
+    return next(); //next invokes the next matching middleware or route handler
+})
 
 app.use((req, res, next) => {
     req.requestTime = Date.now(); //we now have access to requestTime in each of our route handlers
