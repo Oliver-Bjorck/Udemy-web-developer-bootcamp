2378c206e99ca81c27cf4fa2566c927ae2260685
OllyBjorck <bjorckoliver@gmail.com>
2023-12-03 15:35:59 +0000

    added images to the show pages

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 //campground schema
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
