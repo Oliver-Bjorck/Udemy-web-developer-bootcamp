b89f22d200d7e57c8830cd854480b8204f9644bb
OllyBjorck <bjorckoliver@gmail.com>
2024-01-05 20:57:36 +0000

    covered signing cookies

--- Additional-Content/Express-Router-and-Cookies/Cookies/index.js
+++ Additional-Content/Express-Router-and-Cookies/Cookies/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+app.use(cookieParser("thisismysecret"));
 
 app.get("/greet", (req, res) => {
     const {name = "Anonymous"} = req.cookies;
@@ -12,6 +12,17 @@ app.get("/setname", (req, res) => {
     res.cookie("name", "Stevie Chicks");
     res.cookie("animal", "Harlequin Shrimp");
     res.send("Ok, sent you a cookie");
+})
+
+app.get("/getsignedcookie", (req, res) => {
+    res.cookie("fruit", "grape", {signed: true});
+    res.send("ok signed your fruit cookie");
+})
+
+app.get("/verifyfruit", (req, res) => {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.send(req.signedCookies);
 })
 
 app.listen(3000, () => {
