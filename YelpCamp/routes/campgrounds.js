65035612fd48e3200835d8dd05f7ef2af11ea51e
OllyBjorck <bjorckoliver@gmail.com>
2024-01-19 20:30:32 +0000

    refactored routes to use router.route

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -4,25 +4,19 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utilities/catchAsync");
 const {isLoggedIn, isAuthor, validateCampground} = require("../middleware");
 
-//campground index
-router.get("/", catchAsync(campgrounds.index));
+router.route("/")
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
 //create new campground page
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-//creating new campground
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-//individual campground page
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router.route("/:id")
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 //edit individual campground
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-//updating individual campground
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-//delete indivual campground
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -5,17 +5,13 @@ const passport = require("passport");
 const {storeReturnTo} = require("../middleware");
 const users = require("../controllers/users");
 
-//renders registration page
-router.get("/register", users.renderRegister);
+router.route("/register")
+    .get(users.renderRegister)
+    .post(catchAsync(users.createUser))
 
-//registers user
-router.post("/register", catchAsync(users.createUser));
-
-//renders login page
-router.get("/login", users.renderLogin);
-
-//logs user in
-router.post("/login", storeReturnTo, passport.authenticate("local", {failureFlash: true, failureRedirect: "/login"}), users.loginUser);
+router.route("/login")
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate("local", {failureFlash: true, failureRedirect: "/login"}), users.loginUser)
 
 //logs user out
 router.get("/logout", users.logoutUser);
