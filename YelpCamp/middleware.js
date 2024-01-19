dd398debbd18614a0d6fa81d95e1f17d9a9d77f9
OllyBjorck <bjorckoliver@gmail.com>
2024-01-19 18:13:12 +0000

    added more review authorisation

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -1,6 +1,7 @@
 const {campgroundSchema, reviewSchema} = require("./schemas.js");
 const ExpressError = require("./utilities/ExpressError");
 const Campground = require("./models/campground")
+const Review = require("./models/review");
 
 //logged in verification
 module.exports.isLoggedIn = (req, res, next) => {
@@ -34,11 +35,22 @@ module.exports.validateReview = (req, res, next) => {
     }
 }
 
-//author validation
+//campground author validation
 module.exports.isAuthor = async(req, res, next) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     if(!campground.author.equals(req.user._id)) {
+        req.flash("error", "You do not have permission to do that");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+//review author validation
+module.exports.isReviewAuthor = async(req, res, next) => {
+    const {id, reviewId} = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(req.user._id)) {
         req.flash("error", "You do not have permission to do that");
         return res.redirect(`/campgrounds/${id}`);
     }
