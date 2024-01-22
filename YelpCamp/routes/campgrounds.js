443d0ee9567855ac216045f012add2d1367b411b
OllyBjorck <bjorckoliver@gmail.com>
2024-01-22 21:33:10 +0000

    stored uploaded image links to Mongo

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -9,11 +9,7 @@ const upload = multer({storage});
 
 router.route("/")
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.array("image"), (req, res) => {
-        console.log(req.body, req.files);
-        res.send("It Worked!")
-    })
+    .post(isLoggedIn, upload.array("image"), validateCampground, catchAsync(campgrounds.createCampground))
 
 //create new campground page
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
